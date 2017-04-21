f5c1c6fecb8d0f4d6f43d58566c7d6c2087ac396
Dendi Handian <you@example.com>
2017-04-21 20:42:35 +0700

    adding custom.css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
   </head>
   <body>
     <!-- navbar -->
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
